419264ad0647f7264eabde5304671857b090de71
AgustinMazza <agus-mazza@hotmail.com>
2026-05-15 01:16:27 -0300

    Algunos cambios y detalles

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-import 'dotenv/config'
-import express from 'express'
+import "dotenv/config";
+import express from "express";
 
-
-const app = express()
+const app = express();
 
 //constantes
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //motor de plantillas
-app.set('view engine', 'pug')
-app.set('views', './views')
-
-//servidor
-app.listen(PORT, () =>{
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 //rutas
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/fotos',(req,res)=>{
-    res.render('fotos')
-})
+app.get("/fotos", (req, res) => {
+  res.render("fotos");
+});
+
+//servidor
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Error al iniciar servidor: ", err);
+    return;
+  }
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
